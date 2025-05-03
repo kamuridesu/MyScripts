16a13570e4254d1ef0d0c9532a4434fe525d06bc
Luiz Eduardo Amorim Ramos Miranda <luiz.miranda@grupomateus.com>
2025-05-03 11:49:56 -0300

    feat: add makefile install

--- build.sh
+++ build.sh
@@ -6,14 +6,6 @@ if [[ ! -z $1 ]]; then
     TARGET_NAME="$1"
 fi
 
-go_build() {
-    echo "[Go] Searching dependencies..."
-    go mod tidy
-    echo "[Go] Building application '$TARGET_NAME'..."
-    echo "[Go] executing go build -ldflags='-s -w -extldflags \"-static\"' -o $TARGET_NAME"
-    go build -ldflags='-s -w -extldflags "-static"' -o "$TARGET_NAME"
-}
-
 install() {
     if [[ -f "$TARGET_NAME" ]]; then
         echo "Moving '$TARGET_NAME' to '$HOME/.bin/$TARGET_NAME'"
@@ -23,15 +15,50 @@ install() {
     return 1
 }
 
+go_build() {
+    echo "[Go] Searching dependencies..."
+    go mod tidy
+    local MAIN_FILE="main.go"
+    if [[ ! -f "main.go" ]]; then
+        echo "[Go] No main.go found, searching..."
+        MAIN_FILE="./cmd/main.go"
+        if [[ ! -f "./cmd/main.go" ]]; then
+            MAIN_FILE=$(find . -type f -name "main.go" | head -n 1)
+            if [[ -z "$MAIN_FILE" ]]; then
+                echo "No main.go found! Exiting..."
+                return 1
+            fi
+        fi
+        echo "[Go] Found main file in $MAIN_FILE"
+    fi
+    echo "[Go] Building application '$TARGET_NAME'..."
+    echo "[Go] executing go build -ldflags='-s -w -extldflags \"-static\"' -o $TARGET_NAME $MAIN_FILE"
+    go build -ldflags='-s -w -extldflags "-static"' -o "$TARGET_NAME" $MAIN_FILE
+    return $?
+}
+
+make_build() {
+    if [[ -f "configure" ]]; then
+        bash ./configure
+    elif [[ -f "configure.sh" ]]; then
+        bash ./configure.sh
+    fi
+    make build || make install
+    return $?
+}
+
 build() {
     if [[ -f "go.mod" ]]; then
-        go_build
-        return 0
+        go_build || exit 1
+        install || exit 1
+        return $?
+    elif [[ -f "Makefile" ]]; then
+        make_build
+        return $?
     fi
     echo "No suitable build system found."
     return 1
 }
 
 build || exit 1
-install || exit 1
 echo "Done!"
